da71adf684556593ce23b7366e28dcaa892dc38c
Ahmad Salem <ahmadsallam1992@gmail.com>
2025-12-14 19:25:38 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // include all HTML files in your project
+  theme: {
+    extend: {
+      colors: {
+        gold: '#C9A24D',
+        dark: '#0B0B0C',
+      },
+    },
+  },
+  plugins: [],
+}
